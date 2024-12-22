71a33d731efb6fdcdfaac4289eba4ba33955f347
Simon Drolet <simondrolet29@gmail.com>
2024-12-22 18:07:51 +0100

    login works

--- tp2_project/tp2_app/views/login_views.py
+++ tp2_project/tp2_app/views/login_views.py
@@ -1,8 +1,22 @@
+from django.contrib.auth.hashers import check_password
 from django.shortcuts import render
-
+from tp2_app.models import Client, Mechanic
+from django.contrib.auth import authenticate
 
 def login(request):
-    context = {
-        'username': 'Guest',
-    }
-    return render(request, 'login.html', context)
+    message = ""
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        
+        client = Client.objects.filter(username=username).first()
+        mechanic = Mechanic.objects.filter(username=username).first()
+        
+        if client and check_password(password, client.password):
+            message = f"Welcome, {client.username}!"
+        elif mechanic and check_password(password, mechanic.password):
+            message = f"Welcome, {mechanic.username}!"
+        else:
+            message = "Invalid username or password."
+
+    return render(request, 'login.html', {'message': message})
